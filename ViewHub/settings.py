f098c54bcbc9ed8f76216068e048d90c2e19e60a
Ashu <ashutoshsuthar01@gmail.com>
2023-08-03 14:10:35 +0530

    Update settings.py

--- ViewHub/settings.py
+++ ViewHub/settings.py
@@ -163,7 +163,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("0.0.0.0")],
+            "hosts": [("127.0.0.1", 6379)],
         },
     },
 }
